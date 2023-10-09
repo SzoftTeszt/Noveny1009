20401b7e02afd72c37e37f64978fd330f3efdc51
Nagy János <janos@zold.lan>
2023-10-09 12:20:30 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { Environments } from './environments';
+import { NovenyekComponent } from './novenyek/novenyek.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovenyekComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    AngularFireModule.initializeApp(Environments.firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/environments.ts
@@ -0,0 +1,13 @@
+export const Environments = {
+    production:false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBtrh7M6xnPcQ2n2UIy0u5CdLaMF-QIyuo",
+        authDomain: "disznovenyja.firebaseapp.com",
+        databaseURL: "https://disznovenyja-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "disznovenyja",
+        storageBucket: "disznovenyja.appspot.com",
+        messagingSenderId: "445398630246",
+        appId: "1:445398630246:web:71f5b0ac5f998802fc12a7",
+        measurementId: "G-E8TK68C8T4"
+      }
+}
